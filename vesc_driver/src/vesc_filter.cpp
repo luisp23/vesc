0bcb456ab6971f0b4fa68be0ec5f52cfb18922c7
racecar <antwoine31@gmail.com>
2019-10-24 14:05:18 +0200

    changes to work on racecar

--- vesc_driver/src/vesc_filter.cpp
+++ vesc_driver/src/vesc_filter.cpp
@@ -8,6 +8,7 @@ int filter_size;
 float filter_rate; 
 float publisher_rate; 
 float max_vel;
+float car_length;
 
 std::vector<float> linear_vel(50);
 std::vector<float> angular_vel(50);
@@ -15,7 +16,7 @@ std::vector<float> angular_vel(50);
 float avg_linear_vel = 0; 
 float avg_angular_vel = 0; 
 int counter = 0; 
-double length = 0; 
+ 
 
 float odom_linear_velocity = 0;
 geometry_msgs::Twist twist_input; 
@@ -57,8 +58,10 @@ void publisherTimerCallback(const ros::TimerEvent& event){
     std_msgs::Float64 duty_cycle_msg;
     duty_cycle_msg.data = avg_linear_vel/max_vel;
 
-    float radius = odom_linear_velocity/avg_angular_vel;    // use the current velocity of the vehicle to estimate the desired turning radius from desired angular_vel
-    float angle = atan2(length,radius); 
+    float radius = avg_linear_vel/avg_angular_vel;    // use the current velocity of the vehicle to estimate the desired turning radius from desired angular_vel
+    float angle = atan2(car_length,radius); 
+    
+    std::cout << "Radius: " << radius << " Length: " << car_length << "Angle" << angle << std::endl;
 
     std_msgs::Float64 steering_angle_msg;
     steering_angle_msg.data = angle;
@@ -74,14 +77,15 @@ int main(int argc, char** argv){
     private_nh.getParam("filter_size", filter_size);
     private_nh.getParam("filter_rate", filter_rate);
     private_nh.getParam("publisher_rate", publisher_rate);
-    private_nh.getParam("max_vel", max_vel);
+    private_nh.getParam("max_vel", max_vel); 
+    private_nh.getParam("car_length", car_length); 
 
     duty_cycle_pub = nh.advertise<std_msgs::Float64>("commands/motor/duty_cycle", 10);
     steering_angle_pub = nh.advertise<std_msgs::Float64>("commands/motor/position", 10);
     break_signal = nh.advertise<std_msgs::Float64>("commands/motor/brake", 10);
     
     ros::Subscriber odom_sub =  nh.subscribe("odom", 10, &odomCallback);
-    ros::Subscriber twist_sub = nh.subscribe("cmd_vel", 10, &cmdVelCallback);
+    ros::Subscriber twist_sub = nh.subscribe("remote", 10, &cmdVelCallback);
     
     ros::Timer filter_timer_ = nh.createTimer(ros::Duration(1.0/filter_rate), filterTimerCallback);
     ros::Timer publisher_timer = nh.createTimer(ros::Duration(1.0/publisher_rate), publisherTimerCallback);
